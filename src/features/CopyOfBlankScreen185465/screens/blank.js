255c6f9aefb59bcf76c0278857a26a7303da5834
Crowdbotics <team@crowdbotics.com>
2020-07-29 23:02:25 +0000

    Update components

--- src/features/CopyOfBlankScreen185465/screens/blank.js
+++ src/features/CopyOfBlankScreen185465/screens/blank.js
@@ -166,6 +166,36 @@ export class _Blank extends React.Component {
             checked={this.state.Toggle_21}
             onChange={nextChecked => this.setState({ Toggle_21: nextChecked })}
           />
+          <Image
+            resizeMode="cover"
+            source={{
+              uri:
+                "https://d3tklmlrp1a8c2.cloudfront.net/media/project_component_resources/Sign_Up_2_v2.png"
+            }}
+            style={{
+              width: "100%",
+              height: 200,
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 0,
+              marginBottom: 0,
+              paddingLeft: 0,
+              paddingRight: 0,
+              paddingTop: 0,
+              paddingBottom: 0,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0
+            }}
+          />
         </View>
         <View
           style={{
